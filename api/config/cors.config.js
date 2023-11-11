4ebf130fe982553b2fd4ea17ff48c289e6961b60
Carlos del Prado <carlos.delpradomota@telefonica.com>
2023-11-11 12:24:34 +0000

    fix: cors configuration

--- api/config/cors.config.js
+++ api/config/cors.config.js
@@ -1,12 +1,12 @@
 const cors = require('cors');
 
-const allowedOrigins = (process.env.ALLOWED_ORIGINS || "http://localhost:5173")
+const allowedOrigins = (process.env.ALLOWED_ORIGINS || "http://127.0.0.1:5173,http://localhost:5173")
     .split(',')
     .map(origin => origin.trim())
 
-module.exports = cors({ 
+module.exports = cors({
     origin: function (origin, next) {
-        console.log(origin, allowedOrigins)
+        console.debug(`Origin: ${origin} Allowed origins: ${allowedOrigins}`)
         if (allowedOrigins.includes(origin) || origin === undefined) {
             next(null, true)
         } else {
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -43,6 +43,7 @@ const userSchema = new Schema({
             ret.id = ret._id;  
             delete ret._id;   
             delete ret.__v;
+            delete ret.password;
             return ret;
         }
 
